55c74fd5f617030dce7ab181f6c82712b56effe8
JGriff07 <jonathan.griffin.07@gmail.com>
2023-08-06 18:45:55 -0700

    5th Commit - Added Image

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 #Connect that GitHub Repo to Streamlit for hosting services
 
 import streamlit as st
+from PIL import Image
 
 st.header("Streamlit App")
 st.subheader("Programmed in PyCharm, Pushed to GitHub, and Hosted by Streamlit")
 st.write("Hello World! It's great to meet you.")
+
+image = Image.open('sunrise_coastline.png')
+st.image(image, caption='Sunrise on the coast.')
