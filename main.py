6976562b312cb37fb0e0a8f765e0ebf633cfb272
JGriff07 <jonathan.griffin.07@gmail.com>
2023-08-06 17:13:59 -0700

    4th Commit - Added a subheader

--- main.py
+++ main.py
@@ -4,4 +4,5 @@
 import streamlit as st
 
 st.header("Streamlit App")
+st.subheader("Programmed in PyCharm, Pushed to GitHub, and Hosted by Streamlit")
 st.write("Hello World! It's great to meet you.")
